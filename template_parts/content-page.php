880625913bbae77981fde3250f2eb7c78191b3c5
Jan Hindemit <hindemituni@gmail.com>
2023-10-22 13:36:35 +0200

    Delete All

--- template_parts/content-page.php
+++
@@ -1,3 +0,0 @@
-<article class="pt-28" <?php post_class();?>>
-    <h2><?php the_title();?></h2> <?php the_content();?>
-</article>
--- template_parts/content.php
+++
@@ -1,6 +0,0 @@
-<a <?php post_class(" projectCard hover:-translate-y-2 hover:scale-105 hover:last:text-dark duration-200 rounded-xl"); ?>
-    href="<?php the_permalink(); ?>">
-    <img src="<?php get_first_image(get_the_ID()) ?>" alt=""
-        class="w-[340px] h-[180px] sm:h-[220px] rounded-xl object-cover duration-700" />
-    <h1 class="text-white font-bold"><?php the_title(); ?></h1>
-</a>
